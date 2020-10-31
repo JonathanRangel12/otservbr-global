aa3be423b9747dd8d491dc2066550b7583647aaf
darcksody <darcksody@hotmail.com>
2020-10-30 21:25:00 -0600

    Feat: warcores stages

--- data/stages.lua
+++ data/stages.lua
@@ -4,11 +4,48 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 7,
-		multiplier = 2
+		maxlevel = 20,
+		multiplier = 999
 	}, {
-		minlevel = 8,
-		multiplier = 1
+		minlevel = 21,
+		maxlevel = 30,
+		multiplier = 800
+	}, {
+		minlevel = 31,
+		maxlevel = 40,
+		multiplier = 700
+	}, {
+		minlevel = 41,
+		maxlevel = 50,
+		multiplier = 600
+	}, {
+		minlevel = 51,
+		maxlevel = 60,
+		multiplier = 500
+	}, {
+		minlevel = 61,
+		maxlevel = 70,
+		multiplier = 400
+	}, {
+		minlevel = 71,
+		maxlevel = 80,
+		multiplier = 300
+	}, {
+		minlevel = 81,
+		maxlevel = 90,
+		multiplier = 200
+	}, {
+		minlevel = 91,
+		maxlevel = 100,
+		multiplier = 100
+	}, {
+		minlevel = 101,
+		maxlevel = 110,
+		multiplier = 50
+	}, {
+		minlevel = 111,
+		maxlevel = 120,
+		multiplier = 25
 	}
 }
 
